36871facfcdc2ea07730fc716748fd56630125a9
Jirlan Souza <jirlansouza08@gmail.com>
2022-11-10 21:59:58 -0400

    Creted the application theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@stitches/react";
+
+export const Theme = createTheme({
+  colors: {
+    red: "#FF5480",
+    puple: "#9283E0",
+    orange: "#FFA599",
+    yellow: "#FFD581",
+    violet: "#FE90EC",
+    blue: "#27AEFF",
+    green: "#00DC90",
+    background: "#F9F9FB",
+    textDefault: "#000000",
+    textPassive: "#9B9B9B",
+    textQuote: "#9283E0",
+    textHover: "#FF5480",
+  },
+});
